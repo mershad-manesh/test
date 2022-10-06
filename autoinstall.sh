42d836ed4aba1371e78f26307d1c3fb1860a296e
mershad-manesh <105240903+mershad-manesh@users.noreply.github.com>
2022-10-06 12:31:35 -0400

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -3,15 +3,18 @@
 #
 # This script is for testing purposes!!!!
 #
-sudo apt update && sudo apt install -y openjdk-11-jre-headless postgresql postgresql-contrib
+sudo apt-get update && sudo apt-get install -y openjdk-11-jre-headless postgresql postgresql-contrib
 sudo systemctl status postgresql
+sleep 5
 sudo -u postgres psql -c "CREATE USER opennms WITH PASSWORD 'opennms';"
 sudo -u postgres createdb -O opennms opennms
 sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
 sudo systemctl reload postgresql
+sleep 5
 sudo apt-key adv --fetch-keys https://debian.opennms.org/OPENNMS-GPG-KEY
 sudo add-apt-repository -y -s 'deb https://debian.opennms.org stable main'
-sudo DEBIAN_FRONTEND=noninteractive apt -y install opennms r-recommended tree
+sleep 5
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install opennms r-recommended tree
 ##sudo -u opennms vi /usr/share/opennms/etc/opennms-datasources.xml
 echo '--- /usr/share/opennms/etc/opennms-datasources.xml	2022-10-05 14:35:06.290040317 +0000
 +++ /usr/share/opennms/etc/opennms-datasources.xml.backup	2022-10-05 19:01:57.315578426 +0000
@@ -30,4 +33,6 @@ sudo /usr/share/opennms/bin/install -dis
 sudo systemctl daemon-reload
 sudo systemctl restart opennms
 sudo ufw allow 8980/tcp
+sleep 10
+/usr/share/opennms/bin/opennms status
 ##sudo systemctl status opennms
