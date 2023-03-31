4cd98754f65231de88406bc114b16c0a3a13a2a2
mershad-manesh <105240903+mershad-manesh@users.noreply.github.com>
2023-03-31 10:12:47 -0400

    Update database.sh

--- database.sh
+++ database.sh
@@ -23,7 +23,10 @@ sudo -u postgres psql -c "CREATE USER opennms WITH PASSWORD 'opennms';"
 sudo -u postgres createdb -O opennms opennms
 sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
 
-echo "--- /etc/postgresql/10/main/postgresql.conf	2023-03-31 13:35:55.977934006 +0000
+sudo systemctl reload postgresql
+
+cat > /tmp/postgressettings.patch<< EOF
+--- /etc/postgresql/10/main/postgresql.conf	2023-03-31 13:35:55.977934006 +0000
 +++ postgresql.conf	2023-03-31 14:02:26.918918580 +0000
 @@ -56,7 +56,7 @@
  
@@ -34,11 +37,11 @@ echo "--- /etc/postgresql/10/main/postgresql.conf	2023-03-31 13:35:55.977934006 
  					# comma-separated list of addresses;
  					# defaults to 'localhost'; use '*' for all
  					# (change requires restart)
-" > /tmp/postgressettings.patch
+EOF
 
 sudo patch /etc/postgresql/10/main/postgresql.conf /tmp/postgressettings.patch
 
-sudo systemctl reload postgresql
+sudo systemctl restart postgresql
 
 #sleep 20
 export test_pgready=$(sudo -u postgres pg_isready > /dev/null 2>&1; echo $?)
